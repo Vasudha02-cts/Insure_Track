4004bf0d36893fddc8dbbff1675c46de69633aef
geethikabodapati <Bodapati.Geethika@cognizant.com>
2026-02-10 09:33:31 +0530

    Insure Main added Successfully

---
+++ src/main/java/com/insuretrack/identity/InsureTrackApplication.java
@@ -0,0 +1,10 @@
+package com.insuretrack.identity;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class InsureTrackApplication {
+    public static void main(String args[]){
+        SpringApplication.run(InsureTrackApplication.class,args);
+    }
+}
