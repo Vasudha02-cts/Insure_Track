1169225e276266beda23ff4864bb8cc0c14eee7c
geethikabodapati <Bodapati.Geethika@cognizant.com>
2026-02-10 09:35:40 +0530

    Update IdentityServiceApplication.java

--- src/main/java/com/insuretrack/identity/IdentityServiceApplication.java
+++ src/main/java/com/insuretrack/identity/IdentityServiceApplication.java
@@ -1,11 +1,11 @@
-package com.insuretrack.identity;
+// package com.insuretrack.identity;
 
-import org.springframework.boot.SpringApplication;
-import org.springframework.boot.autoconfigure.SpringBootApplication;
+// import org.springframework.boot.SpringApplication;
+// import org.springframework.boot.autoconfigure.SpringBootApplication;
 
-@SpringBootApplication
-public class IdentityServiceApplication {
-    public static void main(String args[]){
-        SpringApplication.run(IdentityServiceApplication.class,args);
-    }
-}
+// @SpringBootApplication
+// public class IdentityServiceApplication {
+//     public static void main(String args[]){
+//         SpringApplication.run(IdentityServiceApplication.class,args);
+//     }
+// }
